9265eeb70173c3f1d1ab8952f3bdb317e8edbe78
rodrigobianchi <rodrigoluizbianchi@gmail.com>
2021-01-28 15:28:49 -0300

    Configure Eureka clients

--- hr-payroll/src/main/java/io/github/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/io/github/hrpayroll/service/PaymentService.java
@@ -4,31 +4,10 @@ import io.github.hrpayroll.entity.Payment;
 import io.github.hrpayroll.entity.Worker;
 import io.github.hrpayroll.feignclient.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
-
-    /* Estratégia de comunicação Rest - Não se caracteriza como microserviço
-    @Value(value = "${hr-worker.host}")
-    private String workerHost;
-
-    @Autowired
-    private RestTemplate restTemplate;
-
-    public Payment getPayment(long workerId, int days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(workerId));
-
-        Worker worker = restTemplate.getForObject(workerHost + "/{id}", Worker.class, uriVariables);
-        return new Payment(worker.getName(), worker.getDailyIncome(), days);
-    }
-     */
 
     @Autowired
     private WorkerFeignClient workerFeignClient;
