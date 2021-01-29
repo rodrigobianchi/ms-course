89e83ce054f8164c0d4769b84668b42fd046cf9d
rodrigobianchi <rodrigoluizbianchi@gmail.com>
2021-01-29 13:52:59 -0300

    configure database user

--- hr-user/src/main/java/io/github/hruser/HrUserApplication.java
+++ hr-user/src/main/java/io/github/hruser/HrUserApplication.java
@@ -1,15 +1,25 @@
 package io.github.hruser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
 
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("BCRYPT = "+ passwordEncoder.encode("123456"));
+	}
 }
