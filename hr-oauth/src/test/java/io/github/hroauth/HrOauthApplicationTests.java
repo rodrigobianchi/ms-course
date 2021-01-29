fd99f537843f68dee71dc439880af161c8934de8
rodrigobianchi <rodrigoluizbianchi@gmail.com>
2021-01-29 14:41:00 -0300

    project hr-oauth created

---
+++ hr-oauth/src/test/java/io/github/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
