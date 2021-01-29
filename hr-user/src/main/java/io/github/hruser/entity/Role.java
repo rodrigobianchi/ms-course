00e29b8f2319a7a00aa82aad986b92446b1e435f
rodrigobianchi <rodrigoluizbianchi@gmail.com>
2021-01-29 12:29:06 -0300

    user role many to many

---
+++ hr-user/src/main/java/io/github/hruser/entity/Role.java
@@ -0,0 +1,40 @@
+package io.github.hruser.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roleName;
+
+    public Role() {
+
+    }
+
+    public Role(Long id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
