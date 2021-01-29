edb729ec1b9e6d7cfcbf54fa8006982819ebf2d0
rodrigobianchi <rodrigoluizbianchi@gmail.com>
2021-01-29 16:54:38 -0300

    login jwt token

--- hr-oauth/src/main/java/io/github/hroauth/entity/User.java
+++ hr-oauth/src/main/java/io/github/hroauth/entity/User.java
@@ -1,10 +1,16 @@
 package io.github.hroauth.entity;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
 
     private Long id;
 
@@ -61,6 +67,37 @@ public class User implements Serializable {
 
     public Set<Role> getRoles() {
         return roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(r ->
+                new SimpleGrantedAuthority(r.getRoleName())).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 }
--- hr-oauth/src/main/java/io/github/hroauth/service/UserService.java
+++ hr-oauth/src/main/java/io/github/hroauth/service/UserService.java
@@ -5,10 +5,13 @@ import io.github.hroauth.feignclient.UserFeignClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -20,6 +23,17 @@ public class UserService {
         if (user == null) {
             logger.error("Email not found: " + email);
             throw new IllegalArgumentException("Email not found");
+        }
+        logger.info("Email found: " + email);
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userFeignClient.searchByEmail(email).getBody();
+        if (user == null) {
+            logger.error("Email not found: " + email);
+            throw new UsernameNotFoundException("Email not found");
         }
         logger.info("Email found: " + email);
         return user;
